ae821820cdd39def9f5b9eb50fcb19f38372b818
dakshvasudev <91746245+dakshvasudev@users.noreply.github.com>
2024-05-29 01:38:09 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by daksh vasudev on 29/05/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
